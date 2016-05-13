adf599346a5bfe542b50e323020df79ab2ec6144
philippe <philtrem90@live.ca>
2016-05-13 09:24:06 -0400

    Ajout du fragment pour les collisions

---
+++ app/src/main/java/com/sensors/philippe/sensorstest/Vue/FragmentCollision.java
@@ -0,0 +1,42 @@
+package com.sensors.philippe.sensorstest.Vue;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sensors.philippe.sensorstest.R;
+
+import java.util.Date;
+
+public class FragmentCollision extends Fragment {
+
+    private String accountID;
+    private Date date;
+    private float strength;
+    private boolean urgencyCalled;
+
+    public FragmentCollision() {
+    }
+
+    public void setDefaultArguements(String accountID, Date date, float strength, boolean urgencyCalled) {
+        this.accountID = accountID;
+        this.date = date;
+        this.strength = strength;
+        this.urgencyCalled = urgencyCalled;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_collision, container, false);
+    }
+
+}
