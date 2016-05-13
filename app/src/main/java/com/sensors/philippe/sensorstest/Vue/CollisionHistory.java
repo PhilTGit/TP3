60bcd18ea8528ace49bb60cd796b245a731af861
vivas5 <felixre-94@hotmail.com>
2016-05-13 14:47:19 -0400

    Support du changement d'orientation dans l'historique de collisions

--- app/src/main/java/com/sensors/philippe/sensorstest/Vue/CollisionHistory.java
+++ app/src/main/java/com/sensors/philippe/sensorstest/Vue/CollisionHistory.java
@@ -13,25 +13,43 @@ import static com.sensors.philippe.sensorstest.R.layout.list_view;
 public class CollisionHistory extends AppCompatActivity {
 
     private static final String INTENT_KEY ="COLLISIONS" ;
+    public static final String COLLISIONS_LIST = "COLLISIONS_LIST";
     private Colision[] colisionsList;
+    private String[] colisionsListFormated = null;
+    private ArrayAdapter<String> adapter;
+    private ListView listViewToHoldCollision;
 //    FragmentManager fragmentManager = getFragmentManager();
 //    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArray(COLLISIONS_LIST,colisionsListFormated);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle extras = getIntent().getExtras();
+        colisionsListFormated = savedInstanceState.getStringArray(COLLISIONS_LIST);
+    if(colisionsListFormated == null) {
+
         if (extras != null) {
-             colisionsList = (Colision[]) extras.get(INTENT_KEY);
-        }
-        String[] colisionsListFormated = new String[colisionsList.length];
-        for(int i =0;i<colisionsList.length;i++){
-            colisionsListFormated[i] = colisionsList[i].toString();
+        colisionsList = (Colision[]) extras.get(INTENT_KEY);
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,list_view,colisionsListFormated);
-        ListView listViewToHoldCollision = (ListView) findViewById(R.id.collision_list);
+        colisionsListFormated = new String[colisionsList.length];
+
+        for (int i = 0; i < colisionsList.length; i++) {
+            colisionsListFormated[i] = colisionsList[i].toString();
+        }
+    }
+        adapter = new ArrayAdapter<String>(this,list_view,colisionsListFormated);
+
+        listViewToHoldCollision = (ListView) findViewById(R.id.collision_list);
+
         if (listViewToHoldCollision != null) {
             listViewToHoldCollision.setAdapter(adapter);
         }
